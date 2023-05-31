f91fadab8d4194fef6bd2538b671a585bd47377a
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-05-31 10:43:50 -0300

    Feat: Footer.jsx
    Footer created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export default function Footer(){
+    return(
+        <Container>
+
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    display: flex;
+    width: 100%;
+    height: 70px;
+    left: 0px;
+    margin-top: 527px;
+    background-color: blue;
+`
--- src/pages/Habits.jsx
+++ src/pages/Habits.jsx
@@ -1,8 +1,12 @@
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function Habits(){
     return(
-        <Header/>
+        <>
+            <Header/>
+            <Footer/>
+        </>
     );
 
 }
