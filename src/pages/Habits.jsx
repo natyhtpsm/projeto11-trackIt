ef3265e5db3fbbd06e547938885102ec8ab53790
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-06-02 19:59:57 -0300

    Update: Habits.jsx
    Added MyHabits component to the Habits page

--- src/pages/Habits.jsx
+++ src/pages/Habits.jsx
@@ -1,13 +1,15 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import styled from 'styled-components';
+import MyHabits from '../components/MyHabits';
 
 export default function Habits(){
     return(
         <Container>
             <Header/>
+            <MyHabits/>
             <Content>
-                
+                <Text>Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</Text>
             </Content>
             <Footer/>
        </Container>
@@ -20,8 +22,23 @@ const Container = styled.div`
     width: 100vw;
     z-index: 1;
     background: #E5E5E5;
+    overflow: hidden;
 `
 
 const Content = styled.div`
-
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`
+const Text = styled.h1`
+    width: 338px;
+    height: 74px;
+    margin-top: 28px;
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 17.976px;
+    line-height: 22px;
+    color: #666666;
 `
