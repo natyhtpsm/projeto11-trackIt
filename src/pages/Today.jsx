f32395a5aa85a43eb4d23666445f9f7d9e802564
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-06-02 22:01:22 -0300

    Feat: Today.jsx
    Created /hoje page

--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,3 +1,45 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import styled from 'styled-components';
+
 export default function Today(){
 
+    return(
+        <Container>
+            <Header/>
+            <Content>
+                <Text>Nenhum hábito concluído ainda</Text>
+            </Content>
+            <Footer/>
+        </Container>
+    );
+    
+    
+
 }
+    
+const Container = styled.div`
+height: 100vh;
+width: 100vw;
+z-index: 1;
+background: #E5E5E5;
+overflow: hidden;
+`
+
+const Content = styled.div`
+height: 100%;
+width: 100%;
+display: flex;
+justify-content: center;
+`
+const Text = styled.h1`
+width: 338px;
+height: 74px;
+margin-top: 28px;
+font-family: 'Lexend Deca';
+font-style: normal;
+font-weight: 400;
+font-size: 17.976px;
+line-height: 22px;
+color: #666666;
+`
