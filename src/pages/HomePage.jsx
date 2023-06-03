0461a66410c3e408e568c64a5650c4b224f25a35
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-06-03 20:50:39 -0300

    HomePage.jsx SignUp.jsx
    Added loading animation to the button and disabled to the inputs

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,94 +4,134 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useState, useContext } from "react";
 import axios from "axios";
 import userContext from '../context/UserContext';
+import { ThreeDots } from 'react-loader-spinner';
 
-export default function HomePage(){
-    const url = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login';
-    const navigate = useNavigate();
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const {userData, setUserData} = useContext(userContext);
+export default function HomePage() {
+  const url = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login';
+  const navigate = useNavigate();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const { userData, setUserData } = useContext(userContext);
+  const [isLoading, setIsLoading] = useState(false); 
+  
+  function receiveData() {
+    const data = { email: email, password: password };
 
-    function receiveData(){
-        const data = {email: email, password: password};
-        const promise = axios.post(url, data);
-        promise.then(response => {
-            const responseData = response.data;
-            console.log(responseData);
-            localStorage.setItem('user', JSON.stringify({
-                token: responseData.token, 
-                image: responseData.image,
-                name: responseData.name,
-            }));
-            setUserData(responseData);
-            console.log(responseData);
-            navigate('/habitos');
-        });
-        promise.catch(erro => console.log(erro.response.data));
-    }
+    setIsLoading(true); 
 
-    return(
-        <Container>
-            <Logo/>
-            <Email data-test="email-input" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-            <Password data-test="password-input" placeholder="senha" value={password} onChange={(e) => setPassword(e.target.value)}/>
-            <Button data-test="login-btn" onClick={receiveData}>
-                <h1>Entrar</h1>
-            </Button>
-            <Link data-test="signup-link" to='/cadastro'>
-                <SignUp>Não tem uma conta? Cadastre-se!</SignUp>
-            </Link>
-        </Container>
-    );
+    axios.post(url, data)
+      .then(response => {
+        const responseData = response.data;
+        console.log(responseData);
+        localStorage.setItem('user', JSON.stringify({
+          token: responseData.token,
+          image: responseData.image,
+          name: responseData.name,
+        }));
+        setUserData(responseData);
+        console.log(responseData);
+        navigate('/habitos');
+      })
+      .catch(error => console.log(error.response.data))
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+  return (
+    <Container>
+      <Logo />
+      <Email
+        data-test="email-input"
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        disabled={isLoading} 
+      />
+      <Password
+        data-test="password-input"
+        placeholder="senha"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        disabled={isLoading} 
+        type="password"
+      />
+      <Button
+        data-test="login-btn"
+        onClick={receiveData}
+        disabled={isLoading} 
+      >
+        {isLoading ? ( 
+          <ThreeDots
+            type="ThreeDots"
+            height={80}
+            width={80}
+            radius={9}
+            color="#4fa94d"
+            ariaLabel="three-dots-loading"
+          />
+        ) : (
+          <h1>Entrar</h1>
+        )}
+      </Button>
+      <Link data-test="signup-link" to="/cadastro">
+        <SignUp>Não tem uma conta? Cadastre-se!</SignUp>
+      </Link>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 const Button = styled.button`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background: #52B6FF;
-    border-radius: 4.63636px;
-    color: white;
-    border: none;
-    width: 303px;
-    height: 45px;
-    h1{
-        font-family: 'Lexend Deca';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 20.976px;
-        line-height: 26px;
-        text-align: center;
-    }
-`
-const Email = styled.input`
-    box-sizing: border-box;
-    width: 303px;
-    height: 45px;
-    background: #FFFFFF;
-    border: 1px solid #D5D5D5;
-    border-radius: 5px;
-    margin-bottom: 6px;
-    margin-top: 0px;
-    ::placeholder {
-        color: #DBDBDB;
-        padding: 11px;
-    }
-`
-const SignUp = styled.h1`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #52b6ff;
+  border-radius: 4.63636px;
+  color: white;
+  border: none;
+  width: 303px;
+  height: 45px;
+  h1 {
     font-family: 'Lexend Deca';
     font-style: normal;
     font-weight: 400;
-    font-size: 13.976px;
-    line-height: 17px;
+    font-size: 20.976px;
+    line-height: 26px;
     text-align: center;
-    text-decoration-line: underline;
-    color: #52B6FF;
-    margin-top: 25px;
-`
-const Password = styled(Email)``
+  }
+`;
+
+const Email = styled.input`
+  box-sizing: border-box;
+  width: 303px;
+  height: 45px;
+  background: #ffffff;
+  border: 1px solid #d5d5d5;
+  border-radius: 5px;
+  margin-bottom: 6px;
+  margin-top: 0px;
+  ::placeholder {
+    color: #dbdbdb;
+    padding: 11px;
+  }
+`;
+
+const SignUp = styled.h1`
+  font-family: 'Lexend Deca';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 13.976px;
+  line-height: 17px;
+  text-align: center;
+  text-decoration-line: underline;
+  color: #52b6ff;
+  margin-top: 25px;
+`;
+
+const Password = styled(Email)``;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,109 +3,156 @@ import Logo from '../components/Logo';
 import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import axios from 'axios';
+import { ThreeDots } from 'react-loader-spinner';
 
-export default function SignUp(){
-    const url = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up';
-    const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [name, setName] = useState('');
-    const [image, setImage] = useState('');
+export default function SignUp() {
+  const url = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up';
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isLoading, setIsLoading] = useState(false); 
 
-    function sendData() {
-        const data = {
-          email: email,
-          name: name,
-          image: image,
-          password: password
-        };
-    
-        axios
-          .post(url, data)
-          .then(response => {
-            const responseData = response.data;
-            console.log(responseData);
-            navigate('/');
+  function sendData() {
+    const data = {
+      email: email,
+      name: name,
+      image: image,
+      password: password
+    };
 
-          })
-          .catch(error => {
-            if (error.response) {
-              console.log(error.response.data); 
-            } else {
-              console.log(error.message); 
-            }
-          });
-    }
-    return(
-        <Container>
-            <Logo/>
-            <Email data-test="email-input" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-            <Password data-test="password-input" placeholder="senha" value={password} onChange={(e) => setPassword(e.target.value)}/>
-            <Name data-test="user-name-input" placeholder="nome" value={name} onChange={(e) => setName(e.target.value)}/>
-            <Picture data-test="user-image-input" placeholder="foto" value={image} onChange={(e) => setImage(e.target.value)}/>
-            <Button data-test="signup-btn" onClick={sendData}>
-                <h1>Cadastrar</h1>
-            </Button>
-            <Link data-test="login-link" to='/'>
-                <Login>Já tem conta? Faça login!</Login>
-            </Link>
+    setIsSubmitting(true);
+    setIsLoading(true); 
 
-        </Container>
-        
-    );
+    axios
+      .post(url, data)
+      .then(response => {
+        const responseData = response.data;
+        console.log(responseData);
+        navigate('/');
+      })
+      .catch(error => {
+        if (error.response) {
+          console.log(error.response.data);
+        } else {
+          console.log(error.message);
+        }
+      })
+      .finally(() => {
+        setIsLoading(false); 
+      });
+  }
+
+  return (
+    <Container>
+      <Logo />
+      <Email
+        data-test="email-input"
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        disabled={isSubmitting}
+      />
+      <Password
+        data-test="password-input"
+        placeholder="senha"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        disabled={isSubmitting}
+        type="password"
+      />
+      <Name
+        data-test="user-name-input"
+        placeholder="nome"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        disabled={isSubmitting}
+      />
+      <Picture
+        data-test="user-image-input"
+        placeholder="foto"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+        disabled={isSubmitting}
+      />
+      <Button
+        data-test="signup-btn"
+        onClick={sendData}
+        disabled={isSubmitting}
+      >
+        {isLoading ? ( 
+          <ThreeDots
+            type="ThreeDots"
+            height={80}
+            width={80}
+            radius={9}
+            color="#4fa94d"
+            ariaLabel="three-dots-loading"
+          />
+        ) : (
+          <h1>Cadastrar</h1>
+        )}
+      </Button>
+      <Link data-test="login-link" to="/">
+        <Login>Já tem conta? Faça login!</Login>
+      </Link>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 const Email = styled.input`
-    box-sizing: border-box;
-    width: 303px;
-    height: 45px;
-    background: #FFFFFF;
-    border: 1px solid #D5D5D5;
-    border-radius: 5px;
-    margin-bottom: 6px;
-    margin-top: 0px;
-    ::placeholder {
-        color: #DBDBDB;
-        padding: 11px;
-    }
-`
+  box-sizing: border-box;
+  width: 303px;
+  height: 45px;
+  background: #ffffff;
+  border: 1px solid #d5d5d5;
+  border-radius: 5px;
+  margin-bottom: 6px;
+  margin-top: 0px;
+  ::placeholder {
+    color: #dbdbdb;
+    padding: 11px;
+  }
+`;
 const Button = styled.button`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 303px;
-    height: 45px;
-    background: #52B6FF;
-    border-radius: 4.63636px;
-    color: white;
-    border: none;
-    h1{
-        width: 64px;
-        height: 26px;
-        font-family: 'Lexend Deca';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 20.976px;
-        line-height: 26px;
-        text-align: center;
-    }
-`
-const Login = styled.h1`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 303px;
+  height: 45px;
+  background: #52b6ff;
+  border-radius: 4.63636px;
+  color: white;
+  border: none;
+  h1 {
+    width: 64px;
+    height: 26px;
     font-family: 'Lexend Deca';
     font-style: normal;
     font-weight: 400;
-    font-size: 13.976px;
-    line-height: 17px;
+    font-size: 20.976px;
+    line-height: 26px;
     text-align: center;
-    text-decoration-line: underline;
-    color: #52B6FF;
-    margin-top: 25px;
-`
-const Password = styled(Email)``
-const Name = styled(Email)``
-const Picture = styled(Email)``
+  }
+`;
+const Login = styled.h1`
+  font-family: 'Lexend Deca';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 13.976px;
+  line-height: 17px;
+  text-align: center;
+  text-decoration-line: underline;
+  color: #52b6ff;
+  margin-top: 25px;
+`;
+const Password = styled(Email)``;
+const Name = styled(Email)``;
+const Picture = styled(Email)``;
